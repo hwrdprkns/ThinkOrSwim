e1752d69793cfbb568ef64079b615f89b990668d
Drew Griffith <wgriffith2@liberty.edu>
2013-06-21 11:42:15 -0400

    Adding Buy and Hold

---
+++ BuyAndHoldSTRATEGY.ts
@@ -0,0 +1,6 @@
+input price = close;
+input month_entry = 6;
+input month_exit = 7;
+
+AddOrder(OrderType.BUY_AUTO, GetMonth() == month_entry and price > Average(price, 50), tickColor = GetColor(1), arrowColor = GetColor(1), name = "LE");
+AddOrder(OrderType.SELL_TO_CLOSE, GetMonth() == month_exit, tickColor = GetColor(2), arrowColor = GetColor(2), name = "LX");

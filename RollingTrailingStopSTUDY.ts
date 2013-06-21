dcde68de43cf4f19c7fd20c21134467cb6454efe
Drew Griffith <wgriffith2@liberty.edu>
2013-06-20 23:06:04 -0400

    Minor Changes to Timeframe

--- RollingTrailingStopSTUDY.ts
+++ RollingTrailingStopSTUDY.ts
@@ -1,7 +1,7 @@
 ##############RollingTrailingStop#############
 # WGRIFFITH2 (c) 2013
 
-input periods = 2;
+input periods = 3;
 def RollingLow = Lowest(data = LOW(), length = periods)[1];
 
 plot StopLoss = RollingLow[-1];

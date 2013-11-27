fe13c39fc69975089de5cc249071923272cb3cf6
Drew Griffith <wgriffith2@liberty.edu>
2013-11-27 11:53:33 -0500

    Major Update to Technical Ranking Study

--- TechnicalRankingSTUDY.ts
+++ TechnicalRankingSTUDY.ts
@@ -4,25 +4,31 @@
 #MODELED AFTER THE TECHNICAL RANKING FROM STOCKCHARTS
 #HTTP://STOCKCHARTS.COM/SCHOOL/DOKU.PHP?ID=CHART_SCHOOL:TECHNICAL_INDICATORS:SCTR
 
+declare lower;
+
 INPUT PRICE = CLOSE;
 
-DEF LTMA = MOVAVGEXPONENTIAL(LENGTH = 200, PRICE = PRICE);
-DEF LTPR = ((PRICE-LTMA)/LTMA);
+DEF EMA = MOVAVGEXPONENTIAL(LENGTH = 200, PRICE = PRICE);
+DEF LTEMA = ((PRICE-EMA)/EMA);
 
 DEF MACD = MACDHistogram("fast length" = 5, "slow length" = 35, "macd length" = 5);
-DEF SLOPE = (LinearRegressionSlope(price = MACD, length = 3));
+def MACD_max = highestAll(MACD)[200];
+DEF invMACD = -((MACD-MACD_max)/MACD_max)*.1;
 
-DEF RSI = # ((RSIWilder()-RSIWilder()[1])/RSIWilder()[1]);
-(100-rsiWilder())/1000;
+def rsi = rsiwilder();
+def rsi_max = highestAll(rsi)[200];
+DEF invRSI = -((rsi-rsi_max)/rsi_max)*.1;
 
 DEF KPERIOD = 14;
 DEF DPERIOD = 3;
 DEF FASTLINE = ROUND(SIMPLEMOVINGAVG(100 * ((CLOSE - LOWEST(LOW, KPERIOD)) / (HIGHEST(HIGH, KPERIOD) - LOWEST(LOW, KPERIOD))), LENGTH = DPERIOD));
-DEF SLOWLINE = ROUND(SIMPLEMOVINGAVG(SIMPLEMOVINGAVG(100*((CLOSE-LOWEST(LOW,KPERIOD))/(HIGHEST(HIGH,KPERIOD)-LOWEST(LOW,KPERIOD))), LENGTH = DPERIOD), LENGTH = DPERIOD));
-DEF STOCH = ((FASTLINE-SLOWLINE)/SLOWLINE)/2;
+def sto_max = highestAll(FASTLINE)[200];
+def invSto = -((fastline-sto_max)/sto_max)*.1;
 
 DEF CMF = chaikinMoneyFlow();
+def CMF_max = highestAll(CMF)[200];
+DEF invCMF = -((CMF-CMF_max)/CMF_max)*.1;
 
-PLOT TOTAL = LTPR+SLOPE+RSI+STOCH+CMF;
-plot p1 = ltpr; plot p2 = slope; plot p3 = rsi; plot p5 = stoch; plot p6 = CMF;
+PLOT TOTAL = LTEMA+invRSI+invMACD+invSto+invCMF;
+
 ##########################################################

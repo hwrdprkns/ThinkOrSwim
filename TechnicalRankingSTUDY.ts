2ed31c9c7aa619d9082ee7b8ebc5637e1fb236de
Drew Griffith <wgriffith2@liberty.edu>
2013-11-27 15:11:49 -0500

    Update to Technical Ranking

--- TechnicalRankingSTUDY.ts
+++ TechnicalRankingSTUDY.ts
@@ -9,22 +9,15 @@ declare lower;
 INPUT PRICE = CLOSE;
 
 DEF EMA = MOVAVGEXPONENTIAL(LENGTH = 200, PRICE = PRICE);
-DEF LTEMA = ((PRICE-EMA)/EMA);
+DEF LTEMA = if ((PRICE-EMA)/EMA)*2 > 1 then 1 else ((PRICE-EMA)/EMA)*2;
 
 DEF MACD = MACDHistogram("fast length" = 5, "slow length" = 35, "macd length" = 5);
-def MACD_max = highestAll(MACD)[200];
-DEF invMACD = -((MACD-MACD_max)/MACD_max)*.1;
+def slopeMACD = linearRegressionSlope(MACD, length = 3);
 
 def rsi = rsiwilder();
 def rsi_max = highestAll(rsi)[200];
-DEF invRSI = -((rsi-rsi_max)/rsi_max)*.1;
+DEF invRSI = -((rsi-rsi_max)/rsi_max);
 
-DEF KPERIOD = 14;
-DEF DPERIOD = 3;
-DEF FASTLINE = ROUND(SIMPLEMOVINGAVG(100 * ((CLOSE - LOWEST(LOW, KPERIOD)) / (HIGHEST(HIGH, KPERIOD) - LOWEST(LOW, KPERIOD))), LENGTH = DPERIOD));
-def sto_max = highestAll(FASTLINE)[200];
-def invSto = -((fastline-sto_max)/sto_max)*.1;
-
-PLOT TOTAL = LTEMA+invRSI+invMACD+invSto;
-
+#PLOT TOTAL = LTEMA+invRSI+invMACD+invSto;
+plot TOTAL = slopeMACD+invRSI+LTEMA;
 ##########################################################

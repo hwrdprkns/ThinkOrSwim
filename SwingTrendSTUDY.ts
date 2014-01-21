8cd5cef156da73dac2e463aca6a240f28dbc7e7b
Drew Griffith <wgriffith2@liberty.edu>
2014-01-21 16:20:41 -0500

    Changing mixed signal to gray instead of blue

--- SwingTrendSTUDY.ts
+++ SwingTrendSTUDY.ts
@@ -1,7 +1,10 @@
-# SWINGTREND
-# WGRIFFITH2 (C) 2013
+# SwingTrend
+# WGRIFFITH2 (c) 2013
 
-def PAINTBARS = yes;
+input price = close;
+input length = 14;
+input sma_length = 12;
+input paintBars = yes;
 
 # STOCHASTICSLOW
 def KPERIOD = 14;
@@ -9,28 +12,25 @@ def DPERIOD = 3;
 def FASTLINE = Round(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD));
 def SLOWLINE = Round(SimpleMovingAvg(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD), LENGTH = DPERIOD));
 
+# RSI
+#def rsi = reference RSIWilder(price = price, length = length);
+#def SMA = SimpleMovingAvg(price = rsi, length = sma_length);
+
 # MACD
-def MACD = MACDHistogram("FAST LENGTH" = 5, "SLOW LENGTH" = 35, "MACD LENGTH" = 5);
+def MACD = MACDHistogram("fast length" = 5, "slow length" = 35, "macd length" = 5);
 
-#def RSI = RSIWilder(PRICE = CLOSE, LENGTH = 14);
-#def SMA = SimpleMovingAvg(PRICE = RSI, LENGTH = 12);
+def GreenPrice = MACD >= 0 and FASTLINE >= SLOWLINE;
+def RedPrice = MACD < 0 and FASTLINE < SLOWLINE;
 
-def GREENPRICE = #RSI >= SMA and 
-MACD >= 0 and 
-FASTLINE >= SLOWLINE;
-def REDPRICE = #RSI < SMA and
-MACD < 0 and
-FASTLINE < SLOWLINE;
-
-plot BULLISH = GREENPRICE;
-plot NEUTRAL = !GREENPRICE and !REDPRICE;
-plot BEARISH = REDPRICE;
+plot Bullish = GreenPrice;
+plot Neutral = !GreenPrice and !RedPrice;
+plot Bearish = RedPrice;
 
 BULLISH.SetDefaultColor(Color.UPTICK);
 BULLISH.SetPaintingStrategy(PaintingStrategy.BOOLEAN_POINTS);
 BULLISH.SetLineWeight(3);
 BULLISH.Hide();
-NEUTRAL.SetDefaultColor(Color.BLUE);
+NEUTRAL.SetDefaultColor(Color.GRAY);
 NEUTRAL.SetPaintingStrategy(PaintingStrategy.BOOLEAN_POINTS);
 NEUTRAL.SetLineWeight(3);
 NEUTRAL.Hide();
@@ -39,9 +39,9 @@ BEARISH.SetPaintingStrategy(PaintingStrategy.BOOLEAN_POINTS);
 BEARISH.SetLineWeight(3);
 BEARISH.Hide();
 
-DefineGlobalColor("BULLISH", Color.UPTICK);
-DefineGlobalColor("NEUTRAL", Color.BLUE);
-DefineGlobalColor("BEARISH", Color.DOWNTICK);
-AssignPriceColor(if !PAINTBARS then Color.CURRENT else if GREENPRICE then GlobalColor("BULLISH") else if REDPRICE then GlobalColor("BEARISH") else GlobalColor("NEUTRAL"));
+DefineGlobalColor("Bullish", Color.UPTICK);
+DefineGlobalColor("Neutral", Color.GRAY);
+DefineGlobalColor("Bearish", Color.DOWNTICK);
+AssignPriceColor(if !paintBars then Color.CURRENT else if GreenPrice then GlobalColor("Bullish") else if RedPrice then GlobalColor("Bearish") else GlobalColor("Neutral"));
 
 #############################################################

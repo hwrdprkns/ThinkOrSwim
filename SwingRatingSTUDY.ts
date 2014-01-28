e039da1b3db94629bd0a7842b152946124b12650
Drew Griffith <wgriffith2@liberty.edu>
2014-01-28 08:38:04 -0500

    Updates to rating logic

--- SwingRatingSTUDY.ts
+++ SwingRatingSTUDY.ts
@@ -12,55 +12,46 @@ def SLOWLINE = Round(SimpleMovingAvg(SimpleMovingAvg(100 * ((close - Lowest(low,
 # MACD
 def MACD = MACDHistogram("FAST LENGTH" = 5, "SLOW LENGTH" = 35, "MACD LENGTH" = 5);
 
-#def RSI = RSIWilder(PRICE = CLOSE, LENGTH = 14);
-#def SMA = SimpleMovingAvg(PRICE = RSI, LENGTH = 12);
-
-def GREENPRICE = #RSI >= SMA and 
+def GREENPRICE = 
 MACD >= 0 and
 FASTLINE >= SLOWLINE;
-def REDPRICE = #RSI < SMA and
+def REDPRICE =
 MACD < 0 and
 FASTLINE < SLOWLINE;
 
 plot BULLISH =
-# BIG MOVE GREEN
+# CONFIRMED MOVE GREEN
 if GREENPRICE
-and Lowest(FASTLINE, 3) < 20
+and Lowest(FASTLINE, 5) < 20
 and close > SimpleMovingAvg()
-and close > close[1]
 then 1
 # TRENDING GREEN
 else if GREENPRICE
 and close > SimpleMovingAvg()
 then .9
-# SMALL MOVE GREEN
+# UNCONFIRMED MOVE GREEN
 ELSE if GREENPRICE
 then .8
-# BIG MOVE BLUE
+# CONFIRMED MOVE BLUE
 else if !GREENPRICE
 and !REDPRICE
-and Lowest(FASTLINE, 3) < 20
+and Lowest(FASTLINE, 5) < 20
 and close > SimpleMovingAvg()
-and close > close[1]
 then .7
 # TRENDING BLUE
 else if !GREENPRICE
 and !REDPRICE
 and close > SimpleMovingAvg()
 then .6
-# SMALL MOVE BLUE
+# UNCONFIRMED MOVE BLUE
 ELSE if !GREENPRICE
 and !REDPRICE
 then .5
 # UNCONFIRMED RED
 ELSE if !GREENPRICE
 and close > close[1]
-and Lowest(FASTLINE, 3) < 20
-then .25
-# OVERSOLD RED
-ELSE if !GREENPRICE
-and Lowest(FASTLINE, 3) < 20
-then .10
+and Lowest(FASTLINE, 5) < 20
+then .15
 else 0;
 
 BULLISH.SetDefaultColor(CreateColor(0, 255, 0));

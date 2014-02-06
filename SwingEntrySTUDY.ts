8e419d660343dd0274d05d70d57f381ed91f23d8
Drew Griffith <wgriffith2@liberty.edu>
2014-02-06 16:36:40 -0500

    Removing Stochastic and MACD from main strategy
    Removing this just from the entry signal. Will still be relied upon to
    make final purchase decision.

--- SwingEntrySTUDY.ts
+++ SwingEntrySTUDY.ts
@@ -4,23 +4,6 @@
 input len1 = 252;
 input len2 = 63;
 
-# STOCHASTICSLOW
-def KPERIOD = 14;
-def DPERIOD = 3;
-def FASTLINE = Round(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD));
-def SLOWLINE = Round(SimpleMovingAvg(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD), LENGTH = DPERIOD));
-
-# MACD
-def MACD = MACDHistogram("FAST LENGTH" = 5, "SLOW LENGTH" = 35, "MACD LENGTH" = 5);
-
-def GREENPRICE =
-MACD >= 0 and
-FASTLINE >= SLOWLINE;
-
-def REDPRICE =
-MACD < 0 and
-FASTLINE < SLOWLINE;
-
 # RSI SR
 def NetChgAvg = WildersAverage(close - close[1], 14);
 def TotChgAvg = WildersAverage(AbsValue(close - close[1]), 14);
@@ -37,19 +20,43 @@ round(LOWEST(RSI,LENGTH=len2), numberOfDigits = 0);
 def rsi_high2 = 
 round(HIGHEST(RSI,LENGTH=len2), numberOfDigits = 0);
 
-plot BULLISH = !REDPRICE
-and close > OHLC4[1]
-AND RSI < 50
+plot CONFIRMEDBULLTWO = close > OHLC4[1]
+AND RSI[1] < 50
 and (RSI[1] - rsi_low1[1] <= 2
 and RSI[1] - rsi_low2[1] <= 2);
 
-plot BEARISH = !GREENPRICE
-and close < OHLC4[1]
-AND RSI > 70
+plot CONFIRMEDBULLONE = close > OHLC4[1]
+AND RSI[1] < 50
+and (RSI[1] - rsi_low1[1] <= 2
+OR RSI[1] - rsi_low2[1] <= 2);
+
+plot UNCONFIRMEDBULL = (RSI < 50
+and (RSI - rsi_low1 <= 1
+and RSI - rsi_low2 <= 1));
+
+plot CONFIRMEDBEARTWO = close < OHLC4[1]
+AND RSI[1] > 70
 and (rsi_high1[1] - RSI[1] <= 2
 and rsi_high2[1] - RSI[1] <= 2);
 
-BULLISH.SetDefaultColor(CreateColor(0, 255, 0));
-BULLISH.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);
-BEARISH.SetDefaultColor(CreateColor(255, 0, 0));
-BEARISH.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);
+plot CONFIRMEDBEARONE = close < OHLC4[1]
+AND RSI[1] > 70
+and (rsi_high1[1] - RSI[1] <= 2
+OR rsi_high2[1] - RSI[1] <= 2);
+
+plot UNCONFIRMEDBEAR = (RSI > 70
+and (rsi_high1 - RSI <= 1
+and rsi_high2 - RSI <= 1));
+
+CONFIRMEDBULLTWO.SetDefaultColor(CreateColor(0, 255, 0));
+CONFIRMEDBULLTWO.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);
+CONFIRMEDBEARTWO.SetDefaultColor(CreateColor(255, 0, 0));
+CONFIRMEDBEARTWO.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);
+CONFIRMEDBULLONE.SetDefaultColor(CreateColor(128, 128, 128));
+CONFIRMEDBULLONE.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);
+CONFIRMEDBEARONE.SetDefaultColor(CreateColor(128, 128, 128));
+CONFIRMEDBEARONE.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);
+UNCONFIRMEDBULL.SetDefaultColor(CreateColor(255, 0, 0));
+UNCONFIRMEDBULL.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);
+UNCONFIRMEDBEAR.SetDefaultColor(CreateColor(0, 255, 0));
+UNCONFIRMEDBEAR.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);

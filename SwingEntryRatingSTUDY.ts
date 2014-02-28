e9afd1301bb9a5f59aaffc8cad18ed463b8301a7
Drew Griffith <wgriffith2@liberty.edu>
2014-02-28 15:22:36 -0500

    Minor update to 2D High/Low

--- SwingEntryRatingSTUDY.ts
+++ SwingEntryRatingSTUDY.ts
@@ -14,22 +14,21 @@ def ROLLINGHIGH = Highest(DATA = high(), LENGTH = periods)[1];
 
 # SMA TEST
 def SMA200 = close > SimpleMovingAvg(LENGTH = 200);
+def SMA50 = close > SimpleMovingAvg(LENGTH = 50);
 def SMA20 = close > SimpleMovingAvg(LENGTH = 20);
 
 plot RATING =
 
 # BULL =
 if
-((SMA200 AND !SMA20)
-OR (!SMA200 AND !SMA20))
-AND RelativeVolume >= 1.0
+SMA200 and !SMA50 and !SMA20
+and RelativeVolume >= 1.0
 and close >= ROLLINGHIGH
 then 1
 
 else if
-((!SMA200 AND SMA20)
-OR (SMA200 AND SMA20))
-AND RelativeVolume >= 1.5
+!SMA200 and SMA50 and SMA20
+and RelativeVolume >= 1.0
 and close <= ROLLINGLOW
 then -1
 
--- SwingEntrySTUDY.ts
+++ SwingEntrySTUDY.ts
@@ -9,23 +9,22 @@ declare upper;
 def RelativeVolume = VolumeAvg(LENGTH = 60) / VolumeAvg(LENGTH = 60).VOLAVG;
 
 # HIGHS / LOWS
-def ROLLINGLOW = Lowest(DATA = low(), LENGTH = periods)[1];
-def ROLLINGHIGH = Highest(DATA = high(), LENGTH = periods)[1];
+def ROLLINGLOW = Lowest(DATA = low, LENGTH = periods)[1];
+def ROLLINGHIGH = Highest(DATA = high, LENGTH = periods)[1];
 
 # SMA TEST
 def SMA200 = close > SimpleMovingAvg(LENGTH = 200);
+def SMA50 = close > SimpleMovingAvg(LENGTH = 50);
 def SMA20 = close > SimpleMovingAvg(LENGTH = 20);
 
 plot BULL =
-SMA200
-AND !SMA20
-AND RelativeVolume >= 1.0
+SMA200 and !SMA50 and !SMA20
+and RelativeVolume >= 1.0
 and close >= ROLLINGHIGH;
 
 plot BEAR =
-!SMA200
-AND SMA20
-AND RelativeVolume >= 1.0
+!SMA200 and SMA50 and SMA20
+and RelativeVolume >= 1.0
 and close <= ROLLINGLOW;
 
 BULL.SetDefaultColor(CreateColor(0, 255, 0));

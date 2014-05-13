a77cf875a7aa385a28a6b2dd95612a7319668cca
Drew Griffith <wgriffith2@liberty.edu>
2014-05-13 16:32:58 -0400

    MAJOR UPDATE: Swing Strategy
    Forgot to save SwingEntrySTUDY.ts in previous commit

--- SwingEntrySTUDY.ts
+++ SwingEntrySTUDY.ts
@@ -1,48 +1,29 @@
 # SWINGENTRY
 # WGRIFFITH2 (C) 2014
 
-declare upper;
-
-input RSISMA_LEN = 12;
+DECLARE UPPER;
 
 # STOCHASTICSLOW
-def KPERIOD = 14;
-def DPERIOD = 3;
-def FASTLINE = Round(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD));
-def SLOWLINE = Round(SimpleMovingAvg(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD), LENGTH = DPERIOD));
+DEF KPERIOD = 14;
+DEF DPERIOD = 3;
+DEF FASTLINE = ROUND(SIMPLEMOVINGAVG(100 * ((CLOSE - LOWEST(LOW, KPERIOD)) / (HIGHEST(HIGH, KPERIOD) - LOWEST(LOW, KPERIOD))), LENGTH = DPERIOD));
+DEF SLOWLINE = ROUND(SIMPLEMOVINGAVG(SIMPLEMOVINGAVG(100 * ((CLOSE - LOWEST(LOW, KPERIOD)) / (HIGHEST(HIGH, KPERIOD) - LOWEST(LOW, KPERIOD))), LENGTH = DPERIOD), LENGTH = DPERIOD));
 
-# MACD
-def MACD = MACDHistogram("FAST LENGTH" = 5, "SLOW LENGTH" = 35, "MACD LENGTH" = 5);
+# TEST
+DEF GREENPRICE = FASTLINE > SLOWLINE;
+DEF REDPRICE = FASTLINE < SLOWLINE;
 
-# OSCILLATOR TEST
-def GREENPRICE = MACD >= 0 and FASTLINE >= SLOWLINE;
-def REDPRICE = MACD < 0 and FASTLINE < SLOWLINE;
+PLOT BULL =
+GREENPRICE
+AND CLOSE >= OPEN
+AND LOWEST(FASTLINE[1],2) < 20;
 
-# RSI
-def NetChgAvg = WildersAverage(close - close[1], 14);
-def TotChgAvg = WildersAverage(AbsValue(close - close[1]), 14);
-def ChgRatio = if TotChgAvg != 0 then NetChgAvg / TotChgAvg else 0;
-def RSI = round(50 * (ChgRatio + 1), numberOfDigits = 0);
+PLOT BEAR =
+REDPRICE
+AND CLOSE <= OPEN
+AND HIGHEST(FASTLINE[1],2) > 80;
 
-def RSISMA = round(SimpleMovingAvg(price = RSI, length = RSISMA_LEN),0);
-
-plot BULL =
-!Redprice
-and !GreenPrice[1]
-and close >= ohlc4[1]
-and RSI <= 60
-and RSI >= RSISMA
-and lowest(FASTLINE[1],3) <= 20;
-
-plot BEAR =
-!GreenPrice
-and !RedPrice[1]
-and close <= ohlc4[1]
-and RSI >= 40
-and RSI <= RSISMA
-and highest(FASTLINE[1],3) >= 80;
-
-BULL.SetDefaultColor(CreateColor(0, 255, 0));
-BULL.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);
-BEAR.SetDefaultColor(CreateColor(255, 0, 0));
-BEAR.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);
+BULL.SETDEFAULTCOLOR(CREATECOLOR(0, 255, 0));
+BULL.SETPAINTINGSTRATEGY(PAINTINGSTRATEGY.BOOLEAN_ARROW_UP);
+BEAR.SETDEFAULTCOLOR(CREATECOLOR(255, 0, 0));
+BEAR.SETPAINTINGSTRATEGY(PAINTINGSTRATEGY.BOOLEAN_ARROW_DOWN);

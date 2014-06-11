03a930fa61f67c5c9904e85b9d6d8c6a99f34314
Drew Griffith <wgriffith2@liberty.edu>
2014-06-10 22:09:31 -0400

    SMA 200 Test; StochSlow (40,3)

--- SwingEntryRatingSTUDY.ts
+++ SwingEntryRatingSTUDY.ts
@@ -5,35 +5,29 @@ declare lower;
 
 input KPERIOD = 40;
 input DPERIOD = 3;
-input RSI_SMA = 10;
 
 # STOCHASTICSLOW
 def FASTLINE = StochasticSlow("D PERIOD" = DPERIOD, "K PERIOD" = KPERIOD);
 def SLOWLINE = StochasticSlow("D PERIOD" = DPERIOD, "K PERIOD" = KPERIOD).SLOWD;
 
-#RSI
-def NETCHGAVG = WildersAverage(close - close[1], 14);
-def TOTCHGAVG = WildersAverage(AbsValue(close - close[1]), 14);
-def CHGRATIO = if TOTCHGAVG != 0 then NETCHGAVG / TOTCHGAVG else 0;
-def RSI = Round(50 * (CHGRATIO + 1), NUMBEROFDIGITS = 0);
-def SMA = Round(SimpleMovingAvg(PRICE = RSI, LENGTH = RSI_SMA), 0);
+def SMA = SimpleMovingAvg(length = 200);
 
 # TEST
-def GREENPRICE = FASTLINE >= SLOWLINE and RSI >= SMA;
-def REDPRICE = FASTLINE < SLOWLINE and RSI <= SMA;
+def GREENPRICE = FASTLINE >= SLOWLINE and close >= SMA;
+def REDPRICE = FASTLINE < SLOWLINE and close <= SMA;
 
 plot RATING =
 
 if
 GREENPRICE
 and close >= OHLC4[1]
-and Lowest(FASTLINE[1], 2) <= 20
+and FASTLINE <= 50
 then 1
 
 else if
 REDPRICE
 and close <= low[1]
-and Highest(FASTLINE[1], 2) >= 80
+and FASTLINE >= 50
 then -1
 
 else 0;
--- SwingEntrySTUDY.ts
+++ SwingEntrySTUDY.ts
@@ -5,34 +5,26 @@ declare upper;
 
 input KPERIOD = 40;
 input DPERIOD = 3;
-input RSI_SMA = 10;
 
 # STOCHASTICSLOW
 def FASTLINE = StochasticSlow("D PERIOD" = DPERIOD, "K PERIOD" = KPERIOD);
 def SLOWLINE = StochasticSlow("D PERIOD" = DPERIOD, "K PERIOD" = KPERIOD).SLOWD;
 
-#RSI
-def NETCHGAVG = WildersAverage(close - close[1], 14);
-def TOTCHGAVG = WildersAverage(AbsValue(close - close[1]), 14);
-def CHGRATIO = if TOTCHGAVG != 0 then NETCHGAVG / TOTCHGAVG else 0;
-
-def RSI = Round(50 * (CHGRATIO + 1), NUMBEROFDIGITS = 0);
-
-def SMA = Round(SimpleMovingAvg(PRICE = RSI, LENGTH = RSI_SMA), 0);
+def SMA = SimpleMovingAvg(length = 200);
 
 # TEST
-def GREENPRICE = FASTLINE >= SLOWLINE and RSI >= SMA;
-def REDPRICE = FASTLINE < SLOWLINE and RSI <= SMA;
+def GREENPRICE = FASTLINE >= SLOWLINE and close >= SMA;
+def REDPRICE = FASTLINE < SLOWLINE and close <= SMA;
 
 plot BULL =
 GREENPRICE
 and close >= OHLC4[1]
-and Lowest(FASTLINE[1], 2) <= 20;
+and FASTLINE <= 50;
 
 plot BEAR =
 REDPRICE
 and close <= low[1]
-and Highest(FASTLINE[1], 2) >= 80;
+and FASTLINE >= 50;
 
 BULL.SetDefaultColor(CreateColor(0, 255, 0));
 BULL.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);

7e5ba1322a4df318ab4c9f3fb674e0e4092a0246
Drew Griffith <wgriffith2@liberty.edu>
2014-07-13 22:06:30 -0400

    Minor: Adding New Targets Study
    Also made plotting mods for GMMA and swing trend studies

---
+++ TargetsSTUDY.ts
@@ -0,0 +1,38 @@
+# TARGETS
+# WGRIFFITH2 (C) 2014
+
+INPUT TARGET_ONE = 20;
+INPUT TARGET_TWO = 40;
+INPUT TARGET_THR = 252;
+
+# PLOT HIGH TARGETS
+PLOT HIGH_ONE = if getLastDay() == getDay() then HIGHEST(DATA = HIGH(), LENGTH = TARGET_ONE)[1]
+else Double.NaN;
+PLOT HIGH_TWO = if getLastDay() == getDay() then HIGHEST(DATA = HIGH(), LENGTH = TARGET_TWO)[1]
+else Double.NaN;
+PLOT HIGH_THR = if getLastDay() == getDay() then HIGHEST(DATA = HIGH(), LENGTH = TARGET_THR)[1]
+else Double.NaN;
+
+# PLOT LOW TARGETS
+PLOT LOW_ONE = if getLastDay() == getDay() then LOWEST(DATA = LOW(), LENGTH = TARGET_ONE)[1]
+else Double.NaN;
+PLOT LOW_TWO = if getLastDay() == getDay() then LOWEST(DATA = LOW(), LENGTH = TARGET_TWO)[1]
+else Double.NaN;
+PLOT LOW_THR = if getLastDay() == getDay() then LOWEST(DATA = LOW(), LENGTH = TARGET_THR)[1]
+else Double.NaN;
+
+HIGH_ONE.SetDefaultColor(CreateColor(255, 255, 255));
+HIGH_ONE.SetPaintingStrategy(PaintingStrategy.POINTS);
+HIGH_TWO.SetDefaultColor(CreateColor(255, 255, 255));
+HIGH_TWO.SetPaintingStrategy(PaintingStrategy.POINTS);
+HIGH_THR.SetDefaultColor(CreateColor(255, 255, 255));
+HIGH_THR.SetPaintingStrategy(PaintingStrategy.POINTS);
+
+LOW_ONE.SetDefaultColor(CreateColor(255, 255, 255));
+LOW_ONE.SetPaintingStrategy(PaintingStrategy.POINTS);
+LOW_TWO.SetDefaultColor(CreateColor(255, 255, 255));
+LOW_TWO.SetPaintingStrategy(PaintingStrategy.POINTS);
+LOW_THR.SetDefaultColor(CreateColor(255, 255, 255));
+LOW_THR.SetPaintingStrategy(PaintingStrategy.POINTS);
+
+##############################################

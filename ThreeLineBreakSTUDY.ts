1b849a8e53b4ae198989632141a60b0d38e578e8
Drew Griffith <wgriffith2@liberty.edu>
2014-07-15 07:30:30 -0400

    Minor: Three Line Break
    Plots are marked with clouds

--- ThreeLineBreakSTUDY.ts
+++ ThreeLineBreakSTUDY.ts
@@ -3,74 +3,60 @@
 
 ## NOT IN PRODUCTION ##
 
-DECLARE UPPER;
-
-DEF PAINTBARS = YES;
-
-DEF STATE = {DEFAULT BARONE, BARTWO, BARTHREE};
-DEF XHIGH;
-DEF XLOW;
-DEF XWHITE;
-SWITCH ( STATE[1] ) {
-  CASE BARONE:
-    STATE    = STATE.BARTWO;
-    XHIGH    = HIGH;
-    XLOW     = LOW;
-    XWHITE   = IF CLOSE > OPEN THEN YES ELSE NO;
-  CASE BARTWO:
-    STATE = STATE.BARTHREE;
-    IF CLOSE > XHIGH[1] THEN {
-      XHIGH  = HIGH;
-      XLOW   = XHIGH[1];
-      XWHITE = YES;
-    } ELSE IF CLOSE < XLOW[1] THEN {
-      XHIGH  = XLOW[1];
-      XLOW   = LOW;
-      XWHITE = NO;
-    } ELSE {
-      XHIGH  = XHIGH[1];
-      XLOW   = XLOW[1];
-      XWHITE = XWHITE[1];
+DEF state = {default barOne, barTwo, barThree};
+DEF xHigh;
+DEF xLow;
+DEF xWhite;
+switch ( state[1] ) {
+case barOne:
+    state    = state.barTwo;
+    xHigh    = close;
+    xLow     = close;
+    xWhite   = if close > open then yes else no;
+case barTwo:
+    state = state.barThree;
+    if close > xHigh[1]
+    then {
+        xHigh  = close;
+        xLow   = xHigh[1];
+        xWhite = yes;
+    } else if close < xLow[1]
+    then {
+        xHigh  = xLow[1];
+        xLow   = close;
+        xWhite = no;
+    } else {
+        xHigh  = xHigh[1];
+        xLow   = xLow[1];
+        xWhite = xWhite[1];
     }
-  CASE BARTHREE:
-    STATE = STATE.BARTHREE;
-    IF CLOSE > MAX(XHIGH[1],XHIGH[2]) THEN {
-      XHIGH  = HIGH;
-      XLOW   = XHIGH[2];
-      XWHITE = YES;
-    } ELSE IF CLOSE < MIN(XLOW[1],XLOW[2]) THEN {
-      XHIGH  = XLOW[2];
-      XLOW   = LOW;
-      XWHITE = NO;
-    } ELSE {
-      XHIGH  = XHIGH[2];
-      XLOW   = XLOW[2];
-      XWHITE = XWHITE[2];
+case barThree:
+    state = state.barThree;
+    if close > Max(xHigh[1], xHigh[2])
+    then {
+        xHigh  = high;
+        xLow   = xHigh[1];
+        xWhite = yes;
+    } else if close < Min(xLow[1], xLow[2])
+    then {
+        xHigh  = xLow[1];
+        xLow   = low;
+        xWhite = no;
+    } else {
+        xHigh  = xHigh[1];
+        xLow   = xLow[1];
+        xWhite = xWhite[1];
     }
 }
-
-DEF TBLH = CLOSE >= XHIGH;
-DEF TBLL = CLOSE <= XLOW;
-DEF NEITHER = !TBLH AND !TBLL;
-
-PLOT BULLISH = TBLH;
-PLOT BEARISH = TBLL;
-PLOT NEUTRAL = NEITHER;
-
-BULLISH.SETDEFAULTCOLOR(COLOR.UPTICK);
-BULLISH.SETPAINTINGSTRATEGY(PAINTINGSTRATEGY.BOOLEAN_POINTS);
-BULLISH.SETLINEWEIGHT(3);
-BULLISH.HIDE();
-NEUTRAL.SETDEFAULTCOLOR(COLOR.BLUE);
-NEUTRAL.SETPAINTINGSTRATEGY(PAINTINGSTRATEGY.BOOLEAN_POINTS);
-NEUTRAL.SETLINEWEIGHT(3);
-NEUTRAL.HIDE();
-BEARISH.SETDEFAULTCOLOR(COLOR.DOWNTICK);
-BEARISH.SETPAINTINGSTRATEGY(PAINTINGSTRATEGY.BOOLEAN_POINTS);
-BEARISH.SETLINEWEIGHT(3);
-BEARISH.HIDE();
-
-DEFINEGLOBALCOLOR("BULLISH", COLOR.UPTICK);
-DEFINEGLOBALCOLOR("NEUTRAL", COLOR.BLUE);
-DEFINEGLOBALCOLOR("BEARISH", COLOR.DOWNTICK);
-ASSIGNPRICECOLOR(IF !PAINTBARS THEN COLOR.CURRENT ELSE IF TBLH THEN GLOBALCOLOR("BULLISH") ELSE IF TBLL THEN GLOBALCOLOR("BEARISH") ELSE GLOBALCOLOR("NEUTRAL"));
+DefineGlobalColor( "3LBH", Color.WHITE );
+DefineGlobalColor( "3LBL", Color.DARK_RED );
+plot TLB_High  = if IsNaN(close) then Double.NaN else if xWhite then xHigh else xLow;
+TLB_High.SetPaintingStrategy( PaintingStrategy.DASHES );
+TLB_High.AssignValueColor( if xWhite then GlobalColor("3LBH") else GlobalColor("3LBL"));
+plot TLB_Low = if IsNaN(close) then Double.NaN else if xWhite then xLow else xHigh;
+TLB_Low.SetPaintingStrategy( PaintingStrategy.DASHES );
+TLB_Low.AssignValueColor( if xWhite then GlobalColor("3LBH") else GlobalColor("3LBL"));
+AddCloud( TLB_High, TLB_Low, color1 = Color.WHITE );
+plot P = close;
+P.SetPaintingStrategy( PaintingStrategy.POINTS );
+P.Hide();

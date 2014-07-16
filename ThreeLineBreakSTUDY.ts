92b4939475a79f71953fa6e12253aeaf484a3417
Drew Griffith <wgriffith2@liberty.edu>
2014-07-15 22:28:43 -0400

    Minor: Three Line Break
    In production and extremely close to what is charted on StockCharts.
    More refinements coming...

--- ThreeLineBreakSTUDY.ts
+++ ThreeLineBreakSTUDY.ts
@@ -1,62 +1,42 @@
 # THREELINEBREAK
 # WGRIFFITH2 (C) 2014
 
-## NOT IN PRODUCTION ##
+def FirstLineHigh = if close[2] > close[3] then close[2] else close[3];
 
-DEF state = {default barOne, barTwo, barThree};
-DEF xHigh;
-DEF xLow;
-DEF xWhite;
-switch ( state[1] ) {
-case barOne:
-    state    = state.barTwo;
-    xHigh    = close;
-    xLow     = close;
-    xWhite   = if close > open then yes else no;
-case barTwo:
-    state = state.barThree;
-    if close > xHigh[1]
-    then {
-        xHigh  = close;
-        xLow   = xHigh[1];
-        xWhite = yes;
-    } else if close < xLow[1]
-    then {
-        xHigh  = xLow[1];
-        xLow   = close;
-        xWhite = no;
-    } else {
-        xHigh  = xHigh[1];
-        xLow   = xLow[1];
-        xWhite = xWhite[1];
-    }
-case barThree:
-    state = state.barThree;
-    if close > Max(xHigh[1], xHigh[2])
-    then {
-        xHigh  = high;
-        xLow   = xHigh[1];
-        xWhite = yes;
-    } else if close < Min(xLow[1], xLow[2])
-    then {
-        xHigh  = xLow[1];
-        xLow   = low;
-        xWhite = no;
-    } else {
-        xHigh  = xHigh[1];
-        xLow   = xLow[1];
-        xWhite = xWhite[1];
-    }
-}
+def FirstLineLow = if close[2] < close[3] then close[2] else close[3];
+
+def SecondLineHigh = if close[1] > FirstLineHigh then close[1] # new high
+                     else FirstLineHigh; # stays the same
+
+def SecondLineLow = if close[1] < FirstLineLow then close[1] # new low
+                    else FirstLineLow; # stays the same
+
+def ThirdLineHigh = if close[0] > max(FirstLineHigh,SecondLineHigh) then close[0] # new high
+                    else if close[0] < min(FirstLineLow,SecondLineLow) then SecondLineHigh # prev high becomes low
+                    else SecondLineHigh; # stays the same
+
+def ThirdLineLow = if close[0] < min(FirstLineLow,SecondLineLow) then close[0] # new low
+                   else if close[0] > max(FirstLineHigh,SecondLineHigh) then SecondLineLow # prev low becomes high
+                   else SecondLineLow; # stays the same
+
+def tlbH = if ThirdLineHigh > max(ThirdLineHigh[1],ThirdLineHigh[2]) then ThirdLineHigh
+           else ThirdLineHigh[1];
+def tblL = if ThirdLineLow < min(ThirdLineLow[1],ThirdLineLow[2]) then ThirdLineLow
+           else ThirdLineLow[1];
+
+def isWhite = if ThirdLineHigh > max(ThirdLineHigh[1],ThirdLineHigh[2]) then yes
+             else if ThirdLineLow < min(ThirdLineLow[1],ThirdLineLow[2]) then no
+             else isWhite[1];
+
 DefineGlobalColor( "3LBH", Color.WHITE );
 DefineGlobalColor( "3LBL", Color.DARK_RED );
-plot TLB_High  = if IsNaN(close) then Double.NaN else if xWhite then xHigh else xLow;
+plot TLB_High = tlbH;
 TLB_High.SetPaintingStrategy( PaintingStrategy.DASHES );
-TLB_High.AssignValueColor( if xWhite then GlobalColor("3LBH") else GlobalColor("3LBL"));
-plot TLB_Low = if IsNaN(close) then Double.NaN else if xWhite then xLow else xHigh;
+TLB_High.AssignValueColor( if isWhite then GlobalColor("3LBH") else GlobalColor("3LBL"));
+plot TLB_Low = tblL;
 TLB_Low.SetPaintingStrategy( PaintingStrategy.DASHES );
-TLB_Low.AssignValueColor( if xWhite then GlobalColor("3LBH") else GlobalColor("3LBL"));
-AddCloud( TLB_High, TLB_Low, color1 = Color.WHITE );
+TLB_Low.AssignValueColor( if isWhite then GlobalColor("3LBH") else GlobalColor("3LBL"));
+#AddCloud( TLB_High, TLB_Low, color1 = Color.WHITE );
 plot P = close;
 P.SetPaintingStrategy( PaintingStrategy.POINTS );
 P.Hide();

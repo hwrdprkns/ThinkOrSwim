83d2395a85fd556846e04c4f6e46e9077337debe
Drew Griffith <wgriffith2@liberty.edu>
2014-09-17 23:22:01 -0400

    Minor: Swing
    Instead of SMA <>, using a closer SR line to determine
    breakout/breakdown

--- SwingTriggerSTUDY.ts
+++ SwingTriggerSTUDY.ts
@@ -3,13 +3,10 @@
 
 declare upper;
 input RSI_LENGTH = 14;
-input RSI_SR = 20;
+input RSI_SR = 8;
 input SMA_LENGTH = 10;
-input OVER_BOUGHT = 50;
-input OVER_SOLD = 50;
-input TRAIL = 2;
-input DISPLACE = 1;
-input AGGREGATIONPERIOD = AggregationPeriod.DAY;
+input OVER_BOUGHT = 60;
+input OVER_SOLD = 40;
 
 # RSI
 def NETCHGAVG = WildersAverage(close - close[1], RSI_LENGTH);
@@ -18,18 +15,17 @@ def CHGRATIO = if TOTCHGAVG != 0 then NETCHGAVG / TOTCHGAVG else 0;
 def RSI = Round(50 * (CHGRATIO + 1), NUMBEROFDIGITS = 0);
 def RSI_HIGH = Round(Highest(RSI, LENGTH = RSI_SR), NUMBEROFDIGITS = 0);
 def RSI_LOW = Round(Lowest(RSI, LENGTH = RSI_SR), NUMBEROFDIGITS = 0);
-def RSI_SMA = Round(SimpleMovingAvg(PRICE = RSI, LENGTH = SMA_LENGTH), NUMBEROFDIGITS = 0);
 def RSI_SUPPORT = (RSI - RSI_LOW) <= 1;
 def RSI_RESISTANCE = (RSI_HIGH - RSI) <= 1;
 
 plot BULL =
-RSI crosses above RSI_SMA and
-RSI_LOW <= OVER_SOLD and
+RSI == RSI_HIGH and
+RSI_HIGH <= OVER_BOUGHT and
 (RSI_SUPPORT[1] or RSI_SUPPORT[2] or RSI_SUPPORT[3]);
 
 plot BEAR =
-RSI crosses below RSI_SMA and
-RSI_HIGH >= OVER_BOUGHT and
+RSI == RSI_LOW and
+RSI_LOW >= OVER_SOLD and
 (RSI_RESISTANCE[1] or RSI_RESISTANCE[2] or RSI_RESISTANCE[3]);
 
 plot RATING =

d932e4b6c6cafffbd4cb62ada5b471bc7fe44fdc
Drew Griffith <wgriffith2@liberty.edu>
2014-10-03 08:52:36 -0400

    Major: Going back to what worked before...
    I went back to determine what I was doing about a year ago to what
    worked really well. I lost my way for a while, but now I am using all
    indicators in conjunction to improve probability of a good entry. I am
    looking for a new high compared to yesterday, crossovers on RSI/RSI SMA,
    Stochastic, and MACD, as well as heavy volume, and candlesticks. The
    volume and candlesticks are not requirements, but they are def
    recommended.

--- SCTRSTUDY.ts
+++ SCTRSTUDY.ts
@@ -1,19 +1,18 @@
 # SCTR
 # DREWGRIFFITH15 (C) 2014
 
-DECLARE LOWER;
+declare lower;
 
-INPUT SMA200 = 200;
-INPUT ROC125 = 125;
-INPUT SMA50 = 50;
-INPUT ROC20 = 20;
-INPUT PPO_HIST = 3;
-INPUT RSI = 14;
-INPUT LT_WEIGHT = .30;
-INPUT MD_WEIGHT = .15;
-INPUT SH_WEIGHT = .05;
-INPUT SHORT_SR = 8;
-INPUT LONG_SR = 40;
+input SMA200 = 200;
+input ROC125 = 125;
+input SMA50 = 50;
+input ROC20 = 20;
+input PPO_HIST = 3;
+input RSI = 14;
+input LT_WEIGHT = .30;
+input MD_WEIGHT = .15;
+input SH_WEIGHT = .05;
+input SR_LENGTH = 40;
 
 # THIS STUDY IS A REPLICATION OF STOCKCHARTS TECHNICAL RANKING
 # http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:sctr
@@ -22,8 +21,8 @@ INPUT LONG_SR = 40;
 #  * Percent above/below 200-day SMA (30%)
 #  * 125-Day Rate-of-Change (30%)
 
-def SM200 = simplemovingavg(close,SMA200);
-def LTSMA = ((Close-SM200)/((Close+SM200)/2));
+def SM200 = SimpleMovingAvg(close, SMA200);
+def LTSMA = ((close - SM200) / ((close + SM200) / 2));
 def LTROC = if close[ROC125] > 0 then (close / close[ROC125] - 1) * 100 else 0;
 def LT = (LTSMA + LTROC) * LT_WEIGHT;
 
@@ -31,8 +30,8 @@ def LT = (LTSMA + LTROC) * LT_WEIGHT;
 #  * Percent above/below 50-day SMA  (15%)
 #  * 20-day Rate-of-Change (15%)
 
-def SM50 = simplemovingavg(close,SMA50);
-def MDSMA = ((Close-SM50)/((Close+SM50)/2));
+def SM50 = SimpleMovingAvg(close, SMA50);
+def MDSMA = ((close - SM50) / ((close + SM50) / 2));
 def MDROC = if close[ROC20] > 0 then (close / close[ROC20] - 1) * 100 else 0;
 def MD = (MDSMA + MDROC) * MD_WEIGHT;
 
@@ -40,29 +39,26 @@ def MD = (MDSMA + MDROC) * MD_WEIGHT;
 #  * 3-day slope of PPO-Histogram (5%)
 #  * 14-day RSI (5%)
 
-def MACD = macDHistogram();
+def MACD = MACDHistogram();
 def SHPPO = LinearRegressionSlope(MACD, length = 3);
-def SHRSI = RSIWilder();
+def SHRSI = reference RSI();
 def SH = (SHPPO + SHRSI) * SH_WEIGHT;
 
-plot SCTR = if round(LT + MD + SH,0) <= 0 then 0 else round(LT + MD + SH, NUMBEROFDIGITS = 1);
+plot SCTR = Round(LT + MD + SH, NUMBEROFDIGITS = 1);
 
-def SHORT_SR_HIGH = ROUND(HIGHEST(SCTR, LENGTH = SHORT_SR), NUMBEROFDIGITS = 1);
-plot LONG_SR_HIGH = ROUND(HIGHEST(SCTR, LENGTH = LONG_SR), NUMBEROFDIGITS = 1);
+plot LONG_SR_HIGH = Round(Highest(SCTR, LENGTH = SR_LENGTH), NUMBEROFDIGITS = 1);
 
-def SHORT_SR_LOW = ROUND(LOWEST(SCTR, LENGTH = SHORT_SR), NUMBEROFDIGITS = 1);
-plot LONG_SR_LOW = ROUND(LOWEST(SCTR, LENGTH = LONG_SR), NUMBEROFDIGITS = 1);
+plot LONG_SR_LOW = Round(Lowest(SCTR, LENGTH = SR_LENGTH), NUMBEROFDIGITS = 1);
 
-SCTR.SETDEFAULTCOLOR(COLOR.GRAY);
-SCTR.SETLINEWEIGHT(3);
-SCTR.ASSIGNVALUECOLOR(IF (SCTR == SHORT_SR_LOW or SCTR == SHORT_SR_HIGH) THEN COLOR.MAGENTA ELSE COLOR.GRAY);
+SCTR.SetDefaultColor(Color.GRAY);
+SCTR.SetLineWeight(3);
 
-LONG_SR_HIGH.SETDEFAULTCOLOR(COLOR.GRAY);
+LONG_SR_HIGH.SetDefaultColor(Color.GRAY);
 LONG_SR_HIGH.SetStyle(Curve.SHORT_DASH);
-LONG_SR_HIGH.SETLINEWEIGHT(1);
+LONG_SR_HIGH.SetLineWeight(1);
 
-LONG_SR_LOW.SETDEFAULTCOLOR(COLOR.GRAY);
+LONG_SR_LOW.SetDefaultColor(Color.GRAY);
 LONG_SR_LOW.SetStyle(Curve.SHORT_DASH);
-LONG_SR_LOW.SETLINEWEIGHT(1);
+LONG_SR_LOW.SetLineWeight(1);
 
 ### $$$ ###
---
+++ SwingAlertSTUDY.ts
@@ -0,0 +1,63 @@
+# SwingAlert
+# DREWGRIFFITH15 (C) 2014
+
+declare upper;
+input KPERIOD = 14;
+input DPERIOD = 3;
+input MACD_FAST = 3;
+input MACD_SLOW = 35;
+input MACD_LENGTH = 3;
+input RSI_LENGTH = 14;
+input RSI_SMA = 10;
+
+# STOCHASTICSLOW
+def FASTLINE = Round(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD));
+def SLOWLINE = Round(SimpleMovingAvg(SimpleMovingAvg(100 * ((close - Lowest(low, KPERIOD)) / (Highest(high, KPERIOD) - Lowest(low, KPERIOD))), LENGTH = DPERIOD), LENGTH = DPERIOD));
+
+# MACD
+def MACD = MACDHistogram("fast length" = MACD_FAST, "slow length" = MACD_SLOW, "macd length" = MACD_LENGTH);
+
+# RSI
+def NETCHGAVG = WildersAverage(close - close[1], RSI_LENGTH);
+def TOTCHGAVG = WildersAverage(AbsValue(close - close[1]), RSI_LENGTH);
+def CHGRATIO = if TOTCHGAVG != 0 then NETCHGAVG / TOTCHGAVG else 0;
+def RSI = Round(50 * (CHGRATIO + 1), NUMBEROFDIGITS = 0);
+def RSISMA = Round(SimpleMovingAvg(PRICE = RSI, LENGTH = RSI_SMA), 0);
+
+# NEW HIGH /LOW
+def NEW_HIGH = close > high[1];
+def NEW_LOW = close < low[1];
+
+# VOLUME
+def VOL_AVG = VolumeAvg().VolAvg;
+def VOL = volume;
+
+plot BULLISH =
+FASTLINE >= SLOWLINE
+AND MACD >= 0
+AND RSI >= RSISMA
+AND NEW_HIGH AND !NEW_HIGH[1]
+and FASTLINE <= 50;
+
+plot BEARISH =
+FASTLINE <= SLOWLINE
+AND MACD <= 0
+AND RSI <= RSISMA
+AND NEW_LOW AND !NEW_LOW[1]
+and FASTLINE >= 50;
+
+plot RATING =
+if BULLISH and VOL > VOL_AVG then 1
+else if BULLISH then .75
+else if BEARISH and VOL > VOL_AVG then .5
+else if BEARISH then .25
+else 0;
+
+BULLISH.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);
+BULLISH.ASSIGNVALUECOLOR(IF RATING == 1 THEN COLOR.Green ELSE COLOR.GRAY);
+
+BEARISH.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);
+BEARISH.ASSIGNVALUECOLOR(IF RATING == .5 THEN COLOR.Red ELSE COLOR.GRAY);
+
+RATING.Hide();
+RATING.HideBubble();

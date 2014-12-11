6c828f24a0068f987aa0dca82c460d56f55db068
Drew Griffith <wgriffith2@liberty.edu>
2014-12-11 08:31:02 -0500

    Adding RSI_SMA requirement for daily swing alert
    Detailed: GMMA inputs modified; Input removed from swing strategies;
    Stochastic must have crossed over and be moving higher. Modifying MACD
    requirement on intraday swing alert; adding moving average cross over to
    intraday swing alert

--- SwingAlertDailySTUDY.ts
+++ SwingAlertDailySTUDY.ts
@@ -2,21 +2,33 @@
 # DREWGRIFFITH15 (C) 2014
 
 declare upper;
-input SLOWK = 14;
-input SLOWD = 3;
-input overbought = 80;
-input oversold = 20;
-INPUT TRAIL = 2;
-INPUT DISPLACE = 1;
-INPUT AGGREGATIONPERIOD = AGGREGATIONPERIOD.DAY;
+def SLOWK = 14;
+def SLOWD = 1;
+def overbought = 80;
+def oversold = 20;
+def RSI_LENGTH = 14;
+def SR_LENGTH = 40;
+def SMA_LENGTH = 10;
+def TRAIL = 2;
+def DISPLACE = 1;
+def AGGREGATIONPERIOD = AGGREGATIONPERIOD.DAY;
 
 # STOCHASTICSLOW
 def FASTLINE = StochasticSlow("k period" = SLOWK, "d period" = SLOWD);
+
+# RSI
+def NETCHGAVG = WildersAverage(close - close[1], RSI_LENGTH);
+def TOTCHGAVG = WildersAverage(AbsValue(close - close[1]), RSI_LENGTH);
+def CHGRATIO = if TOTCHGAVG != 0 then NETCHGAVG / TOTCHGAVG else 0;
+def RSI = Round(50 * (CHGRATIO + 1), NUMBEROFDIGITS = 0);
+def RSI_SMA = Round(SimpleMovingAvg(PRICE = RSI, LENGTH = SMA_LENGTH), 0);
 
 def NEW_HIGH = CLOSE > HIGHEST(HIGH(PERIOD = AGGREGATIONPERIOD), TRAIL)[DISPLACE];
 
 def GreenPrice =
 NEW_HIGH
+and RSI > RSI_SMA
+and FASTLINE > FASTLINE[1]
 and (lowest(FASTLINE,2)[1] <= oversold);
 
 plot BULLISH =
--- SwingAlertIntradaySTUDY.ts
+++ SwingAlertIntradaySTUDY.ts
@@ -2,26 +2,32 @@
 # DREWGRIFFITH15 (C) 2014
 
 declare upper;
-input kperiod = 14;
-input dperiod = 1;
-input overbought = 80;
-input oversold = 20;
-input MA1 = 10;
-input MA2 = 250;
-input averageType = AverageType.EXPONENTIAL;
+def kperiod = 14;
+def dperiod = 1;
+def overbought = 80;
+def oversold = 20;
+def MA1 = 10;
+def MA2 = 250;
+def averageType = AverageType.EXPONENTIAL;
 
 # STOCHASTICSLOW
 def FASTLINE = round(StochasticSlow("k period" = KPERIOD, "d period" = DPERIOD),0);
+
+# MACD
+def MACD = MACDHistogram("fast length" = 5, "slow length" = 35, "macd length" = 5);
 
 # Trying to remove oscillations
 def SMA1 = expAverage(close,MA1);
 def SMA2 = expAverage(close,MA2);
 
 DEF BULLISH =
-((close < SMA1 and close > SMA2)
+SMA1 crosses above SMA2
+or (((close > SMA2)
 or (close > SMA1 and close < SMA2))
+and close > midBodyVal()[1]
 and lowest(FASTLINE,2)[1] <= oversold
-and FASTLINE > oversold;
+and FASTLINE > oversold
+and MACD >= 0);
 
 PLOT BUY = BULLISH;
 

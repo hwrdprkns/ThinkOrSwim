f1ab5538d20570c724eac2e5c594ad97fa09ae48
Drew Griffith <wgriffith2@liberty.edu>
2015-03-19 13:52:46 -0400

    OK
    Adding RSI divergence to SwingAlertIntraday study; Adding short side to
    exit alert

--- ExitAlertSTUDY.ts
+++ ExitAlertSTUDY.ts
@@ -1,13 +1,13 @@
 # EXITALERT
 # DREWGRIFFITH15 (C) 2015
 
+input side = {DEFAULT LONG, SHORT};
+input target = 75; # 75 for long / 25 for short
 input rsi_length = 2;
-input target = 75;
 input price = close;
 
-# On a buy, 30% profit target will be set by default.
-# Otherwise, RSI2 will be used to determine exit.
-# Even on a loss, the RSI2 >= 75 typically indicates a lower high sell point.
+# RSI2 will be used to determine exit.
+# Even on a loss, the RSI typically indicates a lower high sell point.
 # I see this as selling at a high for a stop loss.
 
 # RSI
@@ -16,6 +16,14 @@ def TOTCHGAVG = WildersAverage(AbsValue(price - price[1]), rsi_length);
 def CHGRATIO = if TOTCHGAVG != 0 then NETCHGAVG / TOTCHGAVG else 0;
 def RSI = Round(50 * (CHGRATIO + 1), NUMBEROFDIGITS = 0);
 
-PLOT EXIT = RSI >= TARGET;
+DEF LMT;
+SWITCH (SIDE) {
+CASE LONG:
+    LMT = RSI >= TARGET;
+CASE SHORT:
+    LMT = RSI <= TARGET;
+}
+
+PLOT EXIT = LMT;
 EXIT.SETDEFAULTCOLOR(CREATECOLOR(255, 255, 255));
 EXIT.SETPAINTINGSTRATEGY(PAINTINGSTRATEGY.BOOLEAN_ARROW_DOWN);
--- SwingAlertIntradaySTUDY.ts
+++ SwingAlertIntradaySTUDY.ts
@@ -4,26 +4,40 @@
 declare upper;
 input kperiod = 14;
 input dperiod = 1;
-input oversold = 20;
+input mwoversold = 20;
 input rsi_length = 2;
+input RSIoversold = 5;
+input cor_length = 60;
+input div_filter = 12;
 input price = close;
 
+# Long positions only
+
 # STOCHASTICSLOW
-def FASTLINE = round(StochasticSlow("k period" = KPERIOD, "d period" = DPERIOD),0);
+def FASTLINE = Round(StochasticSlow("k period" = kperiod, "d period" = dperiod), 0);
 
 # RSI
-def NETCHGAVG = WildersAverage(price - price[1], RSI_LENGTH);
-def TOTCHGAVG = WildersAverage(AbsValue(price - price[1]), RSI_LENGTH);
+def NETCHGAVG = WildersAverage(price - price[1], rsi_length);
+def TOTCHGAVG = WildersAverage(AbsValue(price - price[1]), rsi_length);
 def CHGRATIO = if TOTCHGAVG != 0 then NETCHGAVG / TOTCHGAVG else 0;
 def RSI = Round(50 * (CHGRATIO + 1), NUMBEROFDIGITS = 0);
 
-DEF BULLISH =
-RSI[2] <= 5
-and RSI[1] <= 5
-and RSI > 5
-and lowest(FASTLINE,3) <= oversold;
+# RSI Divergence
+def rsi_sma = SimpleMovingAvg(reference RSI(), div_filter);
+def cor = Correlation(close, rsi_sma, cor_length);
 
-PLOT BUY = BULLISH;
+# RSI2 has to be oversold for at least two periods
+# StochasticSlow must be under oversold
+# RSI Divergence must be below 0
+
+def BULLISH =
+RSI[2] <= RSIoversold
+and RSI[1] <= RSIoversold
+and RSI > RSIoversold
+and Lowest(FASTLINE, 3) <= mwoversold
+and cor < 0;
+
+plot BUY = BULLISH;
 
 BUY.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_UP);
-BULLISH.ASSIGNVALUECOLOR(if BULLISH == 1 THEN COLOR.GREEN ELSE IF BULLISH == .5 THEN COLOR.YELLOW ELSE Color.WHITE);
+BUY.AssignValueColor(Color.GREEN);

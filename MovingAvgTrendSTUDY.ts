145f79eca980b3c6920d09301d9451bcaf5cb955
Drew Griffith <wgriffith2@liberty.edu>
2015-11-12 10:19:21 -0500

    OK
    Signals now only looking for price above/below ALL MAs

--- MovingAvgTrendSTUDY.ts
+++ MovingAvgTrendSTUDY.ts
@@ -29,32 +29,20 @@ case "EMA":
     MA5 = Average(close, ultra_long_average);
 }
 
-# define e-signal and crossover point
-def Eup = MA1 > MA2 && MA2 > MA3 && MA4 > MA5;
-def Edn = MA1 < MA2 && MA2 < MA3 && MA4 < MA5;
+# Signals now only looking for price above/below ALL MAs.
+plot signalUP = close > MA1 &&
+                close > MA2 &&
+                close > MA3 &&
+                close > MA4 &&
+                close > MA5 &&
+                close crosses above MA5;
 
-def CrossUp = close > MA1 && Eup && !Eup[1];
-def CrossDn = close < MA1 && Edn && !Edn[1];
-
-# Define up and down signals
-def higherHigh = close > Highest(max(open,close), 3)[1];
-def lowerLow = close < Lowest(min(open,close), 3)[1];
-def SignalUp = if (CrossUp && higherHigh)
-    then 1
-        else if    (CrossUp[1] && higherHigh && !higherHigh[1])
-    then 1
-        else if    (CrossUp[2] && higherHigh && !higherHigh[1] && !higherHigh[2])
-    then 1
-        else Double.NaN;
-
-def SignalDn = if (CrossDn && lowerLow)
-    then 1
-        else if (CrossDn[1] && lowerLow && !lowerLow[1])
-    then 1
-        else if (CrossDn[2] && lowerLow && !lowerLow[1] && !lowerLow[2])
-    then 1
-        else Double.NaN;
-
+plot signalDN = close < MA1 &&
+                close < MA2 &&
+                close < MA3 &&
+                close < MA4 &&
+                close < MA5 &&
+                close crosses below MA5;
 
 # Plot the moving average lines
 plot ln1 = MA1;
@@ -78,13 +66,13 @@ AddVerticalLine(SignalUp && show_vertical_line, "Up", Color.UPTICK);
 AddVerticalLine(SignalDn && show_vertical_line, "Down", Color.LIGHT_RED);
 
 # Show Call / Put Signal in a Chart Bubble
-AddChartBubble(SignalUp && show_bubble_labels, low - 0.3, "Up", Color.UPTICK, no);
-AddChartBubble(SignalDn && show_bubble_labels, high + 0.3, "Dn", Color.LIGHT_RED);
+AddChartBubble(SignalUp && show_bubble_labels, low - 0.2, "Up", Color.UPTICK, no);
+AddChartBubble(SignalDn && show_bubble_labels, high + 0.2, "Dn", Color.LIGHT_RED);
 
 # Add label for Eup or Edn
-AddLabel(SignalUp, "E Up", Color.GREEN);
-AddLabel(SignalDn, "E Dn", Color.RED);
+AddLabel(SignalUp, "Signal Up", Color.GREEN);
+AddLabel(SignalDn, "Signal Down", Color.RED);
 
 # Alerts
-alert(SignalUp, "E Up", "alert type" = Alert.BAR, sound = Sound.Ding);
-alert(SignalDn, "E Dn", "alert type" = Alert.BAR, sound = Sound.Ding);
+alert(SignalUp, "Signal Up", "alert type" = Alert.BAR, sound = Sound.Ding);
+alert(SignalDn, "Signal Down", "alert type" = Alert.BAR, sound = Sound.Ding);
